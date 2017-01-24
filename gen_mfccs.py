c20558c18bd52adbee67cd8dcc44dad23e058843
marc1701 <marc1701@gmail.com>
2017-01-24 11:15:34 +0000

    Added arguments and defaults to MFCC function. Built-in cutting down of MFCC array to 11 coefficients

--- gen_mfccs.py
+++ gen_mfccs.py
@@ -1,24 +1,21 @@
-def gen_mfccs( file ):
+def gen_mfccs( audio, n_filterbanks=26, n_mfccs=11, n_fft=1024, low_freq=200, high_freq=16000 ):
 
-    # MFCC calculation script
+    # need some error handling here to deal with e.g. n_mfccs > n_filterbanks etc.
+
     import numpy as np
     import soundfile as sf
     import scipy.fftpack as fft
 
     # import data and set up loop variables
-    data, fs = sf.read(file)
+    data, fs = sf.read(audio)
     data = data[:,0] # extract left channel (mono input)
     frame_length = int(np.floor(fs/1000) * 25) # calculate samples needed for 25s frame (20-40ms standard)
     step_length = int(np.floor(frame_length/2))
     n_frames = int(np.floor(len(data)/frame_length)) # calculate total number of frames in data 
-    n_filterbanks = 26
-    n_fft = 1024
-    low_freq = 200
-    high_freq = 16000 # these can be changed (maybe these can be user-editable)
 
     # calculate mel filterbank
     mel_lower = 1125 * np.log(1+low_freq/700)
-    mel_upper = 1125 * np.log(1+high_freq/700) # upper and lower bounds mapped to mel scale
+    mel_upper = 1125 * np.log(1+high_freq/700) # upper and lower bounds mapped to mel scale
     filter_mel_centres = np.linspace(mel_lower, mel_upper, n_filterbanks+2) # n linearly spaced filters + 2 frequency points for calculation
     filter_freqs = 700*(np.exp(filter_mel_centres/1125)-1) # map mel frequencies back to standard scale 
     filter_bins = np.floor((n_fft+1)*filter_freqs/fs)
@@ -49,4 +46,8 @@ def gen_mfccs( file ):
         mfccs[:,n] = fft.dct(filter_energies) # mfccs =  DCT of log energy
         # this should build up a matrix of MFCCs for the whole file
         
-        return mfccs
+    mfccs = mfccs[1:n_mfccs+1,:] # keep only specified number of mfccs
+    # need an if statement here to deal with the situation where we want to keep all MFCCs
+    # i.e. mfcc[0,:] should be kept    
+    
+    return mfccs
