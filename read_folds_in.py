d88382a68ddbbbd96391a43e15e5d1249f8444ef
marc1701 <marc1701@gmail.com>
2017-01-27 17:32:02 +0000

    Faffed about with formatting the data. It should now be usable with the scikit GMM module.

--- read_folds_in.py
+++ read_folds_in.py
@@ -3,7 +3,7 @@
 
 from mfcc_audio_class import *
 import sklearn.preprocessing as prp
-from sklearn.mixture import GaussianMixture as gmm
+from sklearn.mixture import GMM
 
 training_info = [] # initialise list for string import
 with open("evaluation_setup/fold1_train.txt", "r") as info_file:
@@ -14,5 +14,29 @@ training_set = [] # initialise list for audio objects
 for x in training_info:
     training_set.append(audio_sample(x[0], x[1]))
 
-for example in training_set: # normalise feature data
-    example.mfccs = prp.scale(example.mfccs)
+# build dictionary and hold accumulated feature vectors from each class label
+# training_data = {}
+# for example in training_set:
+#     if example.label not in training_data:
+#         training_data[example.label] = example.mfccs
+#     else:
+#         training_data[example.label] = np.vstack((training_data[example.label], example.mfccs))
+
+# for soundclass in training_data: # normalise feature data
+#     training_data[soundclass] = prp.scale(training_data[soundclass])
+i = 0
+training_data = {"scene_labels":[], "target_numbers":np.array([], dtype="int"), "data":np.array([])}
+for example in training_set:
+    if training_data["data"].size == 0:
+        training_data["data"] = example.mfccs
+    else:
+        training_data["data"] = np.vstack((training_data["data"], example.mfccs))
+
+    training_data["target_numbers"] = np.append(training_data["target_numbers"], [i] * np.size(example.mfccs, 0))
+    # getting a weird thing at the moment where this ends up being longer than the data array
+
+    if example.label not in training_data["scene_labels"]:
+        training_data["scene_labels"].append(example.label)
+        i += 1
+
+# classifier = GMM(10, "full", )
