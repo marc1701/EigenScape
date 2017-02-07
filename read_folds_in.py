b1dba52566b29538e8eb91af4a2e497163c59fdd
marc1701 <marc1701@gmail.com>
2017-02-07 11:19:10 +0000

    Added in loop for testing training data At the moment data has to be loaded in and MFCCS calculated twice. This seems stupid so the next version will have new data storage.

--- read_folds_in.py
+++ read_folds_in.py
@@ -2,141 +2,90 @@
 # train_text = open("evaluation_setup/fold1_train.txt", "r")
 # training_files = train_text.readlines() # store lines of text file in a list
 
-from mfcc_audio_class import *
+# from mfcc_audio_class import *
 import sklearn.preprocessing as prp
 from sklearn.mixture import GaussianMixture
+import librosa
+import numpy as np
 # import glob
 
 # filepaths = glob.glob(
 # '../TUT-acoustic-scenes-2016-development/evaluation_setup/fold*_train.txt')
 
 # read audio file database text into script
-with open('../TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_train.txt', 'r') as info_file:
+with open(
+'../TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_train.txt',
+'r') as info_file:
     training_info = [line.split() for line in info_file]
 
-# this is a massive fudge - need to make the audio folder an input parameter
-for entry in training_info:
-    entry[0] = '../TUT-acoustic-scenes-2016-development/' + entry[0]
-
-# create audio objects (w/MFCCs) for all the specified audio files
-n_mfccs = 20
-training_set = [audio_sample(x[0], x[1], n_mfccs) for x in training_info]
-
+##########################################################
+####### CALCULATION OF MFCCS #######
+# need to make the audio folder an input parameter
 label_list = [] # make list of numbers for labels
-for example in training_set:
-    if example.label not in label_list:
-        label_list.append(example.label)
+# training_dict = {}
+for entry in training_info:
+    if entry[1] not in label_list:
+        label_list.append(entry[1])
 
-    target = label_list.index(example.label) # numerical class indicator
-    new_data_size = len(example.mfccs)
+    target = label_list.index(entry[1]) # numerical class indicator
+    # load audio file - note librosa collapses to mono and resamples @ 22050 Hz
+    audio, fs = librosa.load(
+                '../TUT-acoustic-scenes-2016-development/' + entry[0])
+    mfccs = librosa.feature.mfcc(audio, fs) # calculate MFCC values
+    # swap axes so feature vectors are horizontal (time runs downwards)
+    mfccs = np.swapaxes(mfccs, 0, 1)
+    new_data_size = len(mfccs)
 
-    # append a target column at the end of the mfcc array
-    data_to_add = np.hstack((example.mfccs, np.array([[target]] * new_data_size)))
+    # append a targets column at the end of the mfcc array
+    data_to_add = np.hstack((mfccs, np.array([[target]] * new_data_size)))
 
-    if 'training_data' not in locals(): # does this exist yet?
+    if 'training_data' not in locals(): # does this variable exist yet?
         training_data = data_to_add
     else:
         training_data = np.vstack((training_data, data_to_add))
+###### MIGHT BE AN IDEA TO TURN THIS INTO A FUCTION ######
+##########################################################
 
-training_data[:,:n_mfccs] = prp.scale(training_data[:,:n_mfccs]) # normalise data
-
-
-
-# # read testing set into memory - REPEAT OF ABOVE LINES, SHOULD FUNCTIONISE THIS
-# with open('../TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_evaluate.txt', 'r') as info_file:
-#     testing_info = [line.split() for line in info_file]
-#
-# for entry in testing_info:
-#     entry[0] = '../TUT-acoustic-scenes-2016-development/' + entry[0]
-
-# create audio objects (w/MFCCs) for all the specified audio files
-# testing_set = [audio_sample(x[0], x[1]) for x in testing_info]
-
-# build dictionary and hold accumulated feature vectors from each class label
-# this is currently imcompatible with the accumulator code below
-# training_data = {}
-# for example in training_set:
-#     if example.label not in training_data:
-#         training_data[example.label] = example.mfccs
-#     else:
-#         training_data[example.label] = np.vstack((training_data[example.label],
-#         example.mfccs))
+# normalise data - scaler object remembers mean and var from training data
+# can apply the same transform later to test data using these values
+data_scaler = prp.StandardScaler()
+# fit scaler and scale training data (exclude last colums - target_numbers)
+scaled_data = data_scaler.fit_transform(training_data[:,:-1])
+# stick target_numbers back on the end
+scaled_data = np.hstack((scaled_data, training_data[:,[-1]]))
 
 gmms = {} # initialise dictionary for GMMs
 results = {} # initialise dictionary for result tallies
 scores = {} # initialise dictionary for scores
 for label in label_list:
-    gmms[label] = GaussianMixture(n_components=50)
+    gmms[label] = GaussianMixture(n_components=10)
     label_num = label_list.index(label)
     # extract class data from training matrix
-    label_training_data = training_data[training_data[:,n_mfccs] == label_num, :n_mfccs]
-    gmms[label].fit(label_training_data) # train GMMs
+    label_data = scaled_data[scaled_data[:,-1] == label_num,:-1]
+    gmms[label].fit(label_data) # train GMMs
     results[label] = [0,0] # set up results dictionary
 
-# for label in training_data:
-#     # normalise feature data
-#     scores[label] = 0
-correct = 0
-for example in training_set:
-    # test_data = prp.scale(example.mfccs)
-    test_data = example.mfccs
-    results[example.label][0] += 1
+# reload audio files individually to test accuracy of classifier
+# (can very probably devise a better way of storing data from earlier to avoid
+#  having to reload all the audio files!)
+for entry in training_info:
+    audio, fs = librosa.load('../TUT-acoustic-scenes-2016-development/' +
+    entry[0])
+
+    test_data = np.swapaxes(librosa.feature.mfcc(audio, fs), 0, 1)
+    test_data = data_scaler.transform(mfccs)
+    results[entry[1]][0] += 1
+
     for label, gmm in gmms.items():
         scores[label] = np.sum(gmm.score_samples(test_data))
 
     top_score = -9e99
     top_label = ''
+
     for label, score in scores.items():
         if top_label == '' or top_score < score:
             top_label = label
             top_score = score
 
-    if top_label == example.label:
-        correct += 1
-        results[example.label][1] += 1
-
-# correct = 0
-# for example in testing_set:
-#     results[example.label][0] += 1 # add to example labels class count
-#     for label, gmm in gmms.items():
-#         scores[label] = np.sum(gmm.score_samples(example.mfccs))
-#
-#     top_score = 0
-#     top_label = ''
-#     for label, score in scores.items():
-#         if top_label == '' or top_score < score:
-#             top_label = label
-#             top_score = score
-#
-#     if top_label == example.label:
-#         results[example.label][1] += 1
-
-# i = -1
-# training_data = {"scene_labels":[], "target_numbers":np.array([], dtype="int"), "data":np.array([])}
-# for example in training_set:
-#     if training_data["data"].size == 0:
-#         training_data["data"] = example.mfccs
-#     else:
-#         training_data["data"] = np.vstack((training_data["data"], example.mfccs))
-#
-#     if example.label not in training_data["scene_labels"]:
-#         training_data["scene_labels"].append(example.label)
-#         i += 1
-#
-#     training_data["target_numbers"] = np.append(training_data["target_numbers"], [i] * np.size(example.mfccs, 0))
-
-# n_classes = len(training_data["scene_labels"])
-# # calculate the means of each class
-# means = np.array([training_data["data"][training_data["target_numbers"]==i].mean(0) for i in range(n_classes)])
-#
-# # create GMM object
-# classifier = GaussianMixture(15, "full", means_init=means)
-#
-# # fit Gaussians to data
-# classifier.fit(training_data["data"])
-#
-# # test model on training data
-# predictions = classifier.predict(training_data["data"])
-# # this hasn't worked very well at all so far
-#
-# accuracy = np.mean(predictions == training_data["target_numbers"]) * 100
+    if top_label == entry[1]:
+        results[entry[1]][1] += 1
