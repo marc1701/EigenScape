3c619ebe389b7709546715d5e186aefade26cb30
marc1701 <marc1701@gmail.com>
2017-03-02 11:00:32 +0000

    Energetic analysis now uses same frames as librosa MFCC calc

--- energetic_analysis.py
+++ energetic_analysis.py
@@ -1,14 +1,11 @@
 # set constants to use in equations (these are approx. correct for air @ 20 C)
-win_len = 20 # 20 ms time-avergaging window
-t_samples = fs/1000 * win_len
-
 Z_0 = 413.3 # characteristic acoustic impedance of air
 rho_0 = 1.2041 # density of air
 c = 343.21 # speed of sound
 
 # filt_audio is indexed [ time : freq : channels ]
-u = -(1 / (Z_0 * np.sqrt(2))) * filt_audio[:,10,1:].T # u = particle velocity
-p = filt_audio[:,10,0] # W-channel of B-format audio = p = sound pressure
+u = -(1 / (Z_0 * np.sqrt(2))) * filt_audio[:,10,1:].T # u = particle velocity (XYZ)
+p = filt_audio[:,10,0] # p = sound pressure (W)
 
 # I = instantaneous sound field intensity
 I = p * u # this will give an array with I at each sample point
@@ -17,13 +14,30 @@ I = p * u # this will give an array with I at each sample point
 E = 0.5 * rho_0 * ( (p**2 / Z_0**2) + (np.linalg.norm(u, axis=0)**2) )
 # this calculates E for each sample point - works accross all
 
-# slice up I into 20 ms windows and calculate means
-I_means = np.array([I_slice.mean(axis=1)
-                        for I_slice in np.split(I, len(E)/t_samples, axis=1)])
 
-# likewise for E
-E_means = np.array([E_slice.mean()
-                        for E_slice in np.split(E, len(E)/t_samples)])
+# for parity with the way librosa calculates MFCCs:
+# librosa.util.frame(np.pad(E, int(n_fft // 2), 'reflect')
+# n_fft = 2048 by default - will have to incorporate a way to make this track
+# whatever values happen to be being used for the MFCC calculation
+# **** It's worth noting that this is essentially actually adding minute
+# extra amounts of time to the beginning and ending of the analysis - since
+# we're in the time domain this might be an issue ****
+
+# old way of calculating I_means - this has no window overlap and no parity
+# with MFCC calculation
+# # slice up I into 20 ms windows and calculate means
+# I_means = np.array([I_slice.mean(axis=1)
+#                         for I_slice in np.split(I, len(E)/t_samples, axis=1)])
+#
+# # likewise for E
+# E_means = np.array([E_slice.mean()
+#                         for E_slice in np.split(E, len(E)/t_samples)])
+
+E_means = np.array([E_frame.mean()
+    for E_frame in librosa.util.frame(np.pad(E, int(2048 // 2), 'reflect')).T])
+
+I_means = np.array([I_frame.mean(axis=0) for I_frame in multichannel_frame(I)])
+# multichannel_frame applies padding automatically
 
 # calculate diffuseness (psi) for each 20 ms slice
 psi = np.array([1 - (np.linalg.norm(I_mean) / (c * E_mean))
@@ -34,18 +48,6 @@ DOA = - I_means
 azi = np.array([np.arctan2(y,x) for x, y, z in DOA])
 elev = np.array([np.arccos(z/np.linalg.norm([x,y,z])) for x, y, z in DOA])
 
-azi = np.arctan(y/x)
-elev = np.arccos(z) # z/r, but we presume vectors are normed so r = 1
-
-# psi = np.zeros(len(E) / t_samples) # this is indexing over only 1 second - wrong but using as test presently
-# for i in range(int(len(E) / t_samples)):
-#     frame_start = i * t_samples
-#     frame_end = (i+1) * t_samples
-#
-#     I_mean = I[:,frame_start:frame_end].mean(axis=1) # might need this independently
-#     E_mean = E[frame_start:frame_end].mean()
-#
-#     psi[i] = 1 - (np.linalg.norm(I_mean) / (c * E_mean))
 
 # it would probably be possible to use matrix multiplication to calculate all
 # values for all frequencies - but I can't hold in my head how to do it and I
